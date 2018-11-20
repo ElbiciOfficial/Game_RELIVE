5aabb938fd0c8e3bc3ab35a669f1014a87879f1c
Charles Kenneth San Miguel <nethiumx@gmail.com>
2018-11-20 21:00:36 +0800

    main menu and minimap

---
+++ LBC_prototype1/Assets/FPS player/Script/UI/BGMScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMScript : MonoBehaviour {
+
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+
+
+	
+	void Start () {
+
+        audio = GetComponent<AudioSource>();
+		
+	}
+	
+	
+	void OnTriggerEnter () {
+
+        if(!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+		
+	}
+}
